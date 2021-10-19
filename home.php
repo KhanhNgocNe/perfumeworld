979d4f474ecccb39cb1c03d6eaa0d6edc893ac99
ngocdkgcc19167 <91665971+ngocdkgcc19167@users.noreply.github.com>
2021-10-19 15:14:07 +0700

    update

--- home.php
+++ home.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Exercise 1 </title>
+    
     <meta charset = " utf - 8 ">
     <meta name = " viewport " content = " width = device - width , initial - scale = 1 ">
     <link rel="stylesheet" href="style.css">
@@ -13,6 +13,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   </head>
   <?php
+  session_start();
 include_once("connection.php");
 ?>
   <body>
