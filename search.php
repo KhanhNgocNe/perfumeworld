2feacb4e3bb631becdb6bfce62653369cc603804
ngocdkgcc19167 <91665971+ngocdkgcc19167@users.noreply.github.com>
2021-11-04 13:48:54 +0700

    Update search.php

--- search.php
+++ search.php
@@ -14,52 +14,7 @@
   </head>
   <body>
 
-    <div class="bg-1">
-    <div class="container">
-      <div class="row text-center">
-      <?php
-      $servername='localhost';
-      $username='root'; // User mặc định là root
-      $password='';
-      $dbname = "data"; // Cơ sở dữ liệu
-      $conn=pg_connect($servername,$username,$password,$dbname);
-      if(!$conn){
-      die('Không thể kết nối Database:' .mysql_error());
-      }
-          if(ISSET($_POST['submit'])){
-              $keyword = $_POST['search'];
-      ?>
-          <h2>Kết quả</h2>
-          <?php
-      
-                // 	include_once("database.php");
-        $result = pg_query($conn, "SELECT * FROM product where title LIKE %$keyword%  ORDER BY title")or die(mysqli_error());
-        while($fetch = mysqli_fetch_array($query)){
-            ?>
-        <?php echo $fetch['title']?>
-        <p><?php echo substr($fetch['content'], 0, 100)?>...</p>
-    <?php
-        }
-        
-          if (!$result) { //add this check.
-            die('Invalid query: ' . pg_error($conn));
-                        }
-          while($row = pg_fetch_array($result,Null, PGSQL_ASSOC)){
-        ?>
-        <div class="col-sm-3">
-          <div class="thumbnail" style="background: #F2F2F2;">
-          <img src="product-imgs/<?php echo $row['pro_image']?>" alt="Product" width="400" height="300">
-            <p><strong><?php echo  $row['product_name']?></strong></p>
-            <p>$<?php echo  $row['price']?></p>
-            <button class="btn">Buy Now</button>
-          </div>
-        </div>
-        <?php
-				}
-				?>
-        </div>
-    </div>
-</div>
-
+    
+ <h1>result</h1>
 </body>
 </html>
