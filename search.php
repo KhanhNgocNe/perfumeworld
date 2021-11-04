73f96d58a80f5059df65371ba8a8ca93f4a5a057
ngocdkgcc19167 <91665971+ngocdkgcc19167@users.noreply.github.com>
2021-11-04 14:26:07 +0700

    Update search.php

--- search.php
+++ search.php
@@ -14,7 +14,49 @@
   </head>
   <body>
 
-    
- <h1>result</h1>
+  <div class="container">
+    <h1><u>All Product</u></h1>
+</div>
+<div class="container">
+    <?php
+    include_once("connection.php");
+    if (isset($_POST["txtSearch"])) {
+        $data = $_POST['txtSearch'];
+          if($data=="")
+          {
+            echo "<script>alert('Please Enter Data!')</script>";
+            echo '<meta http-equiv="refresh" content="0;URL=index.php">';
+          }
+         else {
+      $result = pg_query($conn, "SELECT * FROM product where product_id like '%".$data."%' or product_name like '%".$data."%'");
+if(pg_num_rows($result)==0)
+{
+echo  "<script>alert('No find data. Please Enter Again!')</script>";
+echo '<meta http-equiv="refresh" content="0;URL=index.php">';
+}
+else {
+if (!$result) { //add this check.
+die('Invalid query: ' . mysqli_error($conn));
+}
+else {
+while($row = pgsql_fetch_array($result, Null, MYSQLI_ASSOC)){
+?>
+    <!--Display product-->
+    <div class="col-sm-3">
+          <div class="thumbnail" style="background: #F2F2F2;">
+          <img src="product-imgs/<?php echo $row['pro_image']?>" alt="product" width="400" height="300">
+            <p><strong><?php echo  $row['product_name']?></strong></p>
+            <p>$<?php echo  $row['price']?></p>
+            <a href="?page=cart"><button class="btn">Buy Now</button><a href="?page=cart">
+          </div>
+        </div>
+    <?php
+				}
+      }
+    }
+  }
+}
+?>
+</div>
 </body>
 </html>
