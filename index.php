fb67c5f0a50c51c16e7887844ddd86bdd13e95d3
ngocdkgcc19167 <91665971+ngocdkgcc19167@users.noreply.github.com>
2021-11-04 14:09:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
                     <div class="form-group">
                       <li><input type="text" name="search" class="form-control" placeholder="Search">
                     </div>
-                    <a href="?page=search"><button class="btn btn-default">Search</button></a></li>
+                    <a href="?page=search"><button type="btn-group" name="submit" class="btn btn-default">Search</button></a></li>
                   </ul>
                   <div class="header">
                     <div class="nav navbar-nav navbar-right">
