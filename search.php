ee96e1c91c573b3a85e470fff5060b8db766d962
ngocdkgcc19167 <91665971+ngocdkgcc19167@users.noreply.github.com>
2021-11-04 13:14:27 +0700

    Update search.php

---
+++ search.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Exercise 1 </title>
+    <meta charset = " utf - 8 ">
+    <meta name = " viewport " content = " width = device - width , initial - scale = 1 ">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+    <div class="bg-1">
+    <div class="container">
+      <div class="row text-center">
+      <?php
+                // 	include_once("database.php");
+        $result = pg_query($conn, "SELECT * FROM product where title LIKE '%$keyword%' ORDER BY title");
+        
+          if (!$result) { //add this check.
+            die('Invalid query: ' . pg_error($conn));
+                        }
+          while($row = pg_fetch_array($result,Null, PGSQL_ASSOC)){
+        ?>
+        <div class="col-sm-3">
+          <div class="thumbnail" style="background: #F2F2F2;">
+          <img src="product-imgs/<?php echo $row['pro_image']?>" alt="Product" width="400" height="300">
+            <p><strong><?php echo  $row['product_name']?></strong></p>
+            <p>$<?php echo  $row['price']?></p>
+            <button class="btn">Buy Now</button>
+          </div>
+        </div>
+        <?php
+				}
+				?>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
