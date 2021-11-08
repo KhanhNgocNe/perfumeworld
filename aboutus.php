dda594209fa290b6fd5b31ed4e588c74643a38f3
ngocdkgcc19167 <91665971+ngocdkgcc19167@users.noreply.github.com>
2021-11-08 09:22:44 +0700

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -8,30 +8,19 @@
 </head>
 <body>
 
-<h1 id="title">PERFUMEWORLD – ABOUT US</h1>
+<h1 id="title">BARBIE DOLL – ABOUT US</h1>
 <div class="body">
-<p>In February 2018, SF PerfumeWorld Cologne realised its project and opened the first store exclusively with niche fragrances.</p>
+<p>Barbie is a fashion doll manufactured by the  ATN company and launched in March 1959.</p>
 <br>
 
-<p>Should you be in search for an exclusive fragrance, you are exactly right here. We distribute niche fragrances with passion and attach great
-importance to offering our customers not only online shopping but also the possibility to immerse themselves in the magical world of scents 
-directly on site. Sustainability, respect for creation and striving for harmony determine the thinking and actions of SF Perfume History.
-We are looking forward to meeting you and we always endeavor to enhance your daily life one more time with wonderful moments. Spoil yourself 
-with our exclusive, premium and luxury perfumes!</p>
+<p>Barbie is the figurehead of a brand of Mattel dolls and accessories, including other family members and collectible dolls.
+     Barbie has been an important part of the toy fashion doll market for over sixty years, and has been the subject of numerous 
+     controversies and lawsuits, often involving parodies of the doll and her lifestyle. Mattel has sold over a billion Barbie 
+     dolls, making it the company's largest and most profitable line</p>
 
-<p>A scent can tell a lot about its owner: herb, fresh, bright, dark or powdery scents expose in a sparkling way the character,
-the soul… Who wants to be more individual will decide for a niche perfume.</p>
-
-<p>In case of every niche fragrance, you can feel the passion which is hiding behind. Each niche perfume tells its own story.
-The fragrances express atmosphere, love stories and emotions. Niche fragrances have exclusive ingredients and are associated with
-a very selective distribution as they are produced in small quantities. The flacons are designed in a very special way as well…
-You can often see dark shades. Nothing superfluous. Some perfumers engrave the name of the fragrance or decorate the flacon with different materials:
-gemstones, wooden elements, leather etc. Quality is always the first priority. These perfumes are for customers who attach importance to something special.</p>
-
-<p>Just visit our online shop, take a look at our recommendations for you and be enchanted by luxury. Should you need any assistance or be in search for inspiration,
-the SF Perfume History team is happy to support. We assist you in the search for specific criteria, such as concentration, gender, occasion or specific ingredients.
-We offer an assortment of the best niche fragrances for discerning customers. Should you not be able to find a specific fragrance, just write us an e-mail or call us.
-We are at your disposal at any time.</p>
+<p>Barbie and her boyfriend Ken have been described as two most popular dolls in the world.
+     The doll has transformed the toy business in affluent communities worldwide by becoming a 
+     vehicle for the sale of related merchandise (accessories, clothes, friends of Barbie, etc.)</p>
 </div>
 
 </body>
